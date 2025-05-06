e9d6b475730c82eaba32a174b91dbfa5c4b1da69
samba ba <ba269962@gmail.com>
2025-05-06 02:33:50 +0000

    page d'accueil par défaut

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [MatToolbarModule, MatCardModule, MatButtonModule, MatIconModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
